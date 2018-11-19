e393b0312279b39f020d443c1f7e815b5097fcae
WeiFeng Guo <weifeng.guo@cnexlabs.com>
2018-11-19 19:46:10 +0800

    int_decompose.c

---
+++ cprogram/int_decompose.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int x;
+	scanf("%d",&x);
+	int digit;
+	int ret = 0;
+	while(x>0){
+		digit = x%10;
+		printf("%d\n",digit);
+		ret = ret*10 + digit;
+		//printf("%d",ret); 
+		x /=10;
+	}
+
+}
